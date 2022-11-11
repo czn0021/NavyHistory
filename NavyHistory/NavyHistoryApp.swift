5a38c75e68a01632820fb777a9db97d7384f215b
czn0021 <czn0021@auburn.edu>
2022-11-11 12:13:15 -0600

    Initial Commit

---
+++ NavyHistory/NavyHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavyHistoryApp.swift
+//  NavyHistory
+//
+//  Created by Christopher Noveron-Nunez on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NavyHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
